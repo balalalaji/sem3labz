1189c01c77eb81bd220655b9727b55be480b4e5a
mvbalaji <114253293+balalalaji@users.noreply.github.com>
2023-01-02 11:26:26 +0530

    Update q1.c

--- DSA/Lab5/q1/q1.c
+++ DSA/Lab5/q1/q1.c
@@ -1,57 +1,82 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-typedef struct
-{
-    int *a;
-    int tos;
-} stack;
+typedef struct{
+	char* stack;
+	int top;
+}STACK;
 
-stack push(stack s, int x)
-{
-    s.tos++;
-    s.a[s.tos] = x;
-    return s;
+int isEmpty(STACK* s){
+	if(s->top == -1)
+		return 1;
+	return 0;
 }
 
-stack delete (stack s, int ind)
-{
-    int i;
-    for (i = ind; i < s.tos; i++)
-        s.a[i] = s.a[i + 1];
-    s.tos--;
-    return s;
+int isFull(STACK* s,int size){
+	if(s->top == size - 1)
+		return 1;
+	return 0;
 }
 
-int main()
-{
-    stack s;
-    int n, k;
-    printf("Enter number of elements ");
-    scanf("%d", &n);
-    s.a = (int *)malloc(n * sizeof(int));
-    for (int i = 0; i < n; i++)
-    {
-        printf("Enter element no. %d ", (i + 1));
-        scanf("%d", &s.a[i]);
-    }
-    s.tos = n - 1;
-    printf("Enter the number of elements that must be deleted ");
-    scanf("%d", &k);
-    while (k > 0)
-    {
-        for (int i = 0; i <= s.tos - 1; i++)
-        {
-            if (s.a[i] < s.a[i + 1])
-            {
-                s = delete (s, i);
-                k--;
-                break;
-            }
-        }
-    }
-    printf("The final array is:\n");
-    for (int i = 0; i <= s.tos; i++)
-        printf("%d ", s.a[i]);
-    return 0;
-}   
+void push(STACK* s, char ele,int size){
+	if(isFull(s,size)){
+		printf("\nOVERFLOW ! CANNOT PUSH\n");
+		return;
+	}
+	s->stack[++s->top] = ele;
+}
+
+char pop(STACK* s){
+	if(isEmpty(s)){
+		printf("\nUNDERFLOW ! CANNOT POP\n");
+		return -1;
+	}
+	return s->stack[s->top--];	
+}
+
+void display(STACK* s){
+	if(isEmpty(s)){
+		printf("\nStack is empty !\n");
+		return;
+	}
+	for(int i = 0; i < s->top + 1; i++)
+		printf("%c",s->stack[i]);
+}
+
+int main(){
+	STACK *ps,s;
+	ps = &s;
+	char ele;
+	int n,choice;
+	printf("Enter the size of stack: ");
+	scanf("%d",&n);
+	ps->stack = (char*)malloc(n*sizeof(char));
+	ps->top = -1;
+	while(1){
+		printf("\n1.PUSH\n");
+		printf("2.POP\n");
+		printf("3.DISPLAY\n");
+		printf("4.EXIT\n");
+		printf("\nEnter your choice: ");
+		scanf("%d",&choice);
+		if(choice == 1){
+			printf("Enter the element you want to push:");
+			scanf(" %c",&ele);
+			push(ps,ele,n);
+		}
+		else if(choice == 2){
+			char x;
+			if((x=pop(ps)) == -1){} 
+			else{
+				printf("Popped element is %c",x);
+			}
+		}
+		else if(choice == 3){
+			display(ps);
+		}
+		else{
+			break;
+		}
+	}
+	return 0;
+}
